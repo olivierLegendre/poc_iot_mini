d97969e320e0bc1ac20870e3e57d726fbe79f33e
olivier.legendre <olivier.legendre.pro@gmail.com>
2026-01-21 08:50:59 +0100

    config elements

--- scripts/20_generate_mqtt_auth.sh
+++ scripts/20_generate_mqtt_auth.sh
@@ -14,10 +14,15 @@ source ./.env
 set +a
 
 PASSFILE="$ROOT/stack/mosquitto/passwordfile"
-rm -f "$PASSFILE"
-touch "$PASSFILE"
-chmod 600 "$PASSFILE"
 
-docker run --rm -v "$PASSFILE:/pwfile" eclipse-mosquitto:2.0   sh -c "mosquitto_passwd -b /pwfile '$MQTT_ADMIN_USER' '$MQTT_ADMIN_PASS' &&          mosquitto_passwd -b /pwfile '$MQTT_INGEST_USER' '$MQTT_INGEST_PASS' &&          mosquitto_passwd -b /pwfile '$MQTT_CONTROL_USER' '$MQTT_CONTROL_PASS'"
+sudo rm -f "$PASSFILE"
+sudo touch "$PASSFILE"
+sudo chown root:root "$PASSFILE"
+sudo chmod 600 "$PASSFILE"
+
+docker run --rm -v "$PASSFILE:/pwfile" eclipse-mosquitto:2.0 \
+  sh -c "mosquitto_passwd -b /pwfile '$MQTT_ADMIN_USER' '$MQTT_ADMIN_PASS' && \
+         mosquitto_passwd -b /pwfile '$MQTT_INGEST_USER' '$MQTT_INGEST_PASS' && \
+         mosquitto_passwd -b /pwfile '$MQTT_CONTROL_USER' '$MQTT_CONTROL_PASS'"
 
 echo "Generated mosquitto passwordfile: $PASSFILE"
